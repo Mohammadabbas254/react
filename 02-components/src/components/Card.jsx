2f0a569998fc350e830fa24f449f3812b2b2d8a2
Mohammadabbas254 <mohammadabbasiiit@gmail.com>
2025-12-16 14:07:31 +0530

    02 components

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <div className="card">
+            <h1>Mohammad Abbas</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, sit!</p>
+        </div>
+    </div>
+  )
+}
+
+export default Card
