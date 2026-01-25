a1180330a83f317813e714b0d7b2adf99976bfca
Mohammadabbas254 <mohammadabbasiiit@gmail.com>
2026-01-26 00:00:18 +0530

    # completed LeftContent icon and text

--- 07-ui-project/src/Components/Section1/LeftContent.jsx
+++ 07-ui-project/src/Components/Section1/LeftContent.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import 'remixicon/fonts/remixicon.css'
+
 
 const LeftContent = () => {
   return (
-    <div className='h-full w-1/4 bg-blue-600'>
-
+    <div className='h-full flex flex-col justify-between w-1/3'>
+        <div className='p-6'>
+            <h3 className='mb-7 text-7xl font-bold'>Prospective <br/><span>Customer</span><br/> Segementation</h3>
+            <p className='text-x font-medium text-gray-600'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio error earum aut mollitia quae eaque distinctio dolorem voluptatem! Praesentium, est.</p>
+        </div>
+        <div className="text-9xl">
+            <i className="ri-arrow-right-up-line"></i>
+        </div>
     </div>
   )
 }
--- 07-ui-project/src/Components/Section1/RightContent.jsx
+++ 07-ui-project/src/Components/Section1/RightContent.jsx
@@ -3,10 +3,7 @@ import React from 'react'
 const RightContent = () => {
   return (
     <div className='h-full w-3/4 bg-blue-600'>
-        <div className=""></div>
-        <div className="">
-            <i className="ri-arrow-right-up-line"></i>
-        </div>
+        
     </div>
   )
 }
