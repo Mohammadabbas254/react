f4d8c7bcd0b1feb109b3627c8805dff53bfaef89
Mohammadabbas254 <mohammadabbasiiit@gmail.com>
2026-01-29 23:16:45 +0530

    FIX: Changed the button colcor according to application theme

--- 07-ui-project/src/Components/Section1/RightCardContent.jsx
+++ 07-ui-project/src/Components/Section1/RightCardContent.jsx
@@ -7,8 +7,8 @@ const RightCardContent = (props) => {
         <div>
             <p className='text-xl leading-relaxed text-white mb-14'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi ad nemo nisi quibusdam, velit possimus.</p>
             <div  className='flex justify-between'>
-                <button className='bg-blue-600 text-white font-medium px-8 py-2 rounded-full'>{props.tag}</button>
-                <button  className='bg-blue-600 text-white font-medium px-3 py-2 rounded-full'><i className='ri-arrow-right-line'></i></button>
+                <button className='bg-gray-600 text-white font-medium px-8 py-2 rounded-full'>{props.tag}</button>
+                <button  className='bg-gray-600 text-white font-medium px-3 py-2 rounded-full'><i className='ri-arrow-right-line'></i></button>
             </div>
         </div>
     </div>
