f56f555cd35edad5af35962bdf40732ad17be475
Mohammadabbas254 <mohammadabbasiiit@gmail.com>
2026-02-04 22:22:48 +0530

    HOOKS: useState array added

--- 09-usestate/src/App.jsx
+++ 09-usestate/src/App.jsx
@@ -3,15 +3,17 @@ import React, { useState } from 'react'
 const App = () => {
   const [num, setNum] = useState(20);
   const [name, setName] = useState('Boss')
+  const [numArr, setNumArr] = useState([1,2,3])
   function changeNum(){
     console.log(num);
     
     setNum(30);
     setName('JK')
+    setNumArr([3,4,5])
   }
   return (
     <div>
-      <h1>Value of a is {num} <br/> Value of name is {name}</h1>
+      <h1>Value of a is {num} <br/> Value of name is {name} <br /> Array is {numArr}</h1>
       <button onClick={changeNum}>Click Me</button>
     </div>
   )
