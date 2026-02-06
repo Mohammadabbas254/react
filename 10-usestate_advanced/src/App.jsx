21b237f81508a54701dcd79672e140425ed5e69f
Mohammadabbas254 <mohammadabbasiiit@gmail.com>
2026-02-06 23:08:38 +0530

    Working with advanced concepts of useState with objects + arrays

--- 10-usestate_advanced/src/App.jsx
+++ 10-usestate_advanced/src/App.jsx
@@ -10,12 +10,22 @@ const App = () => {
     const newNum = {...num};
     newNum.user = 'mOHAMMAD'
     setNum(newNum)
+
+  }
+  const [num2, setNum2] = useState([10, 20, 30])
     
+  const btnClicked2 = () =>{
+    const newNum2 = [...num2]
+    newNum2.push(99)
+    setNum2(newNum2)
   }
   return (
     <div>
       <h1>{num.user}, {num.age}</h1>
       <button onClick={btnClicked}>click</button>
+
+      <h1>{num2}</h1>
+      <button onClick={btnClicked2}>click</button>
     </div>
   )
 }
