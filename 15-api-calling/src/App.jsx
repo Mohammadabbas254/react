5c2fcd0e4cb541680a6b68287d1490ce63be3327
Mohammadabbas254 <mohammadabbasiiit@gmail.com>
2026-03-05 00:01:38 +0530

    15-api-calling using axios getting data by clicking Get Data button and see results in the console

--- 15-api-calling/src/App.jsx
+++ 15-api-calling/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import axios from 'axios'
 const App = () => {
   const getData = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-    const data = await response.json()
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users')
     
-    console.log(data);
+    console.log(response.data);
+    
   }
   return (
     <div>
