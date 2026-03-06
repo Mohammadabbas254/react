e58f3b40ca10b3c0645d248ede77f4cfb62fe7a0
Mohammadabbas254 <mohammadabbasiiit@gmail.com>
2026-03-06 22:07:37 +0530

    Get data from api and use in UI

--- 15-api-calling/src/App.jsx
+++ 15-api-calling/src/App.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 const App = () => {
+  const [data, setData] = useState([])
   const getData = async () => {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+    const response = await axios.get('https://picsum.photos/v2/list')
     
-    console.log(response.data);
+    setData(response.data)
     
   }
   return (
     <div>
       <button onClick={getData}>Get Data</button>
+      <div>
+        {data.map(function(elem, idx){
+          return <h3>Hello, {elem.author} {idx}</h3>
+        })}
+      </div>
     </div>
   )
 }
