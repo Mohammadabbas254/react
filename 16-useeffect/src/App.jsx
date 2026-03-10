a28a7282f9e488aa223cbfd63004e30b5ec9b473
Mohammadabbas254 <mohammadabbasiiit@gmail.com>
2026-03-10 22:04:07 +0530

    16-Useeffect: example for useeffect on mouse enter dependency

--- 16-useeffect/src/App.jsx
+++ 16-useeffect/src/App.jsx
@@ -1,14 +1,26 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
 
+  const [num, setNum] = useState(0)
+  const [num2, setNum2] = useState(100)
+  
   useEffect(function(){
     console.log("use effect is running...");
-  })
-  
+  }, [num])
+
   return (
     <div>
+      <h1>num {num}</h1>
+      <h1>num2 {num2}</h1>
 
+      <button onMouseEnter={()=>{
+        setNum(num+1)
+      }
+      } onMouseLeave={()=>
+        setNum2(num2+1)
+
+      }>click</button>
     </div>
   )
 }
