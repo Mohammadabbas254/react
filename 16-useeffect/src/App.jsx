68c5e507e3a1fade985421368b0da2cb68853d84
Mohammadabbas254 <mohammadabbasiiit@gmail.com>
2026-03-10 21:47:22 +0530

    16-Useeffect: example for useeffect

---
+++ 16-useeffect/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+
+const App = () => {
+
+  useEffect(function(){
+    console.log("use effect is running...");
+  })
+  
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
