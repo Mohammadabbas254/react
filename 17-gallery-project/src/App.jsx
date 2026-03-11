804d862ecc15a58dd6486f0c98e3ee354b75bd57
Mohammadabbas254 <mohammadabbasiiit@gmail.com>
2026-03-11 22:21:39 +0530

    !7-Gallery Project: getting the data from lorem picsum api and showing it on UI by clicking Get data button

---
+++ 17-gallery-project/src/App.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+
+const App = () => {
+
+  const [userData, setUserData] = useState([]);
+  
+  const getData = async () => {
+    const response = await axios.get('https://picsum.photos/v2/list?page=3&limit=30')
+    
+    setUserData(response.data)
+    
+    console.log(response.data)
+    
+  }
+
+  let printUserData = 'No user Available'
+
+  if(userData.length>0){
+    printUserData = userData.map(function(elem, idx){
+      return <div>
+        <div className='h-40 w-44 overflow-hidden bg-white rounded-xl'>
+        <img className='h-full w-full object-cover' src={elem.download_url} alt='' />
+      </div>
+      <h2>{elem.author}</h2>
+      </div>
+    })
+  }
+
+  return (
+    <div className='bg-black overflow-auto h-screen p-4 text-white'>
+      <button 
+      onClick={getData}
+        className='bg-green-600 active:scale-95 mb-3 px-5 py-3 rounded text-white'>
+        get data
+      </button>
+
+      <div className='flex flex-wrap gap-4'>
+        {printUserData}
+      </div>
+    </div>
+  )
+}
+
+export default App
