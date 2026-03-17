7fcb337a607f07b01e5c38bdf3516677929bcd61
Mohammadabbas254 <mohammadabbasiiit@gmail.com>
2026-03-17 21:15:30 +0530

    17-GalleryApp on reload only will get data without any button by useEffect

--- 17-gallery-project/src/App.jsx
+++ 17-gallery-project/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 
 const App = () => {
@@ -14,26 +14,34 @@ const App = () => {
     
   }
 
-  let printUserData = 'No user Available'
+  useEffect(function(){
+    getData()
+  },[])
+
+  let printUserData = <h3 className="text-gray-400 text-xs">No user Available</h3>
 
   if(userData.length>0){
     printUserData = userData.map(function(elem, idx){
-      return <div>
-        <div className='h-40 w-44 overflow-hidden bg-white rounded-xl'>
-        <img className='h-full w-full object-cover' src={elem.download_url} alt='' />
+      
+      return <div key={idx} target='_blank'>
+        <a href={elem.url}>
+          <div className='h-40 w-44 overflow-hidden bg-white rounded-xl'>
+          <img className='h-full w-full object-cover' src={elem.download_url} alt='' />
+          </div>
+          <h2 className='font-bold text-lg'>{elem.author}</h2>
+        </a>
       </div>
-      <h2>{elem.author}</h2>
-      </div>
+
     })
   }
 
   return (
     <div className='bg-black overflow-auto h-screen p-4 text-white'>
-      <button 
+      {/* <button 
       onClick={getData}
         className='bg-green-600 active:scale-95 mb-3 px-5 py-3 rounded text-white'>
         get data
-      </button>
+      </button> */}
 
       <div className='flex flex-wrap gap-4'>
         {printUserData}
